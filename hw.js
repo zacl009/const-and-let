51c0ffb25597cb335b0cb77ca4b1fd447f752708
Zac Lopez <zachary.lopez@student.swic.edu>
2020-02-03 11:49:29 -0600

    Added practice file

---
+++ hw.js
@@ -0,0 +1,10 @@
+const myString = "salkdhfoihewoiflkjsdj";
+let myNum = 23;
+
+myString = "something else";
+myNum = 23;
+console.log(myNum);
+
+const stringTest = "This is a string";
+const numTest = 123;
+const booleanTest = true;
